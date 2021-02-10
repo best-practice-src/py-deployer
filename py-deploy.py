f87a40910a29437ac09d27c8e94f285922c8bc00
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-02-10 19:00:10 +0100

    fixed license

--- py-deploy.py
+++ py-deploy.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 """
 *********************************************************************************
 *                                                                               *
-* arguments.py -- Argument parsing.                                             *
+* py-deploy.py -- Main executable.                                              *
 *                                                                               *
 ********************** IMPORTANT PY-DEPLOYER LICENSE TERMS **********************
 *                                                                               *
@@ -23,38 +25,7 @@
 *********************************************************************************
 """
 
-from . import settings
-from argparse import ArgumentParser, Namespace
+from app.main import main
 
-
-def get_arguments() -> Namespace:
-    """
-    Return the input arguments
-    :return: A Namespace
-    """
-    arg_parser = __get_argument_parser()
-    args = arg_parser.parse_args()
-
-    if args.version:
-        print('v' + str(settings.VERSION))
-        arg_parser.exit(0)
-
-    if not args.stage:
-        arg_parser.print_help()
-        arg_parser.exit(0)
-
-    return args
-
-
-def __get_argument_parser() -> ArgumentParser:
-    """
-    Make a new ArgumentParser
-    :return: An ArgumentParser
-    """
-    argument_parser = ArgumentParser(
-        description="[OF - Institutional] City Coverage Update v"+str(settings.VERSION)+" (Py Microservice)\n"
-    )
-    # --- Optional arguments ---#
-    argument_parser.add_argument("-v", "--version", help="show program's version number and exit", action="store_true")
-    argument_parser.add_argument("-s", "--stage", help="The stage to deploy", type=str)
-    return argument_parser
+if __name__ == "__main__":
+    main()
